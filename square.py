46db255bb2e498ebd7945dcda6bdcfa58e9c8b80
fathimathsafwana <fathimathsafwanatayattuchira@gmail.com>
2025-11-15 15:09:15 +0530

    Update square.py

--- square.py
+++ square.py
@@ -1,6 +1,6 @@
-for num in range(1000, 10000):
-    root=int(num ** 0.5)
-    if root *root==num:
-        digits_even=all(int(d)%2==0 for d in str(num))
-        if digits_even:
-            print(num)
+n=int(input("enter total numbers:"))
+squares=[]
+for i in range(n):
+    num=int(input("enter number"))
+    squares.append(num*num)
+print("squares:",squares)
